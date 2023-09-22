f5b78631077f156e881938f5cdf72f5ac7b13cfa
Hirwa Arnold <arnold.hirw@gmail.com>
2023-09-22 02:31:47 +0200

    Omit to use buffer

--- _putchar.c
+++ _putchar.c
@@ -4,39 +4,26 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
- * Description: _putchar uses a local buffer of 1024 to call write
- * as little as possible
  */
 int _putchar(char c)
 {
-	static char buf[1024];
-	static int i;
-
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buf, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buf[i] = c;
-		i++;
-	}
-	return (1);
+	return (write(1, &c, 1));
 }
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
- * Return: number of chars written
+ * _puts - prints string
+ * @str: string
+ * Return: number of characters printed
  */
 int _puts(char *str)
 {
-	register int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (; str[i]; i++)
 		_putchar(str[i]);
+
 	return (i);
 }
